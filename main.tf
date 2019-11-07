1b8d287a6cb898a06df6fb71fd1c36e6c981a349
Krista LaFentres <lafentres@users.noreply.github.com>
2019-11-07 16:19:24 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
+data "terraform_remote_state" "random-length" {
+  backend = "remote"
+  config = {
+    organization = var.organization
+    workspaces = {
+      name = var.length-workspace
+    }
+  }
+}
+
 resource "random_string" "separator" {
   keepers = {
     uuid = uuid()
   }
-  length  = 1
+  length  = data.terraform_remote_state.random-length.outputs.integer
   special = true
   upper   = false
   lower   = false
