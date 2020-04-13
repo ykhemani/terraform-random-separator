25d2e88dcd45512003a27b037253ff7aa7bc896f
ykhemani <ykhemani@users.noreply.github.com>
2020-04-13 14:17:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 data "terraform_remote_state" "random-length" {
   backend = "remote"
   config = {
+    hostname = var.tfe_hostname
     organization = var.organization
     workspaces = {
       name = var.length-workspace
